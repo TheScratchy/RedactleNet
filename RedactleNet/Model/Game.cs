5b33d8d2e0e78abfe8b0107146c79556de80009d
Marcin <Marcin@DESKTOP-MFASAGD>
2023-02-05 19:14:43 +0100

    Add project files.

---
+++ RedactleNet/Model/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedactleNet.Model
+{
+    internal class Game
+    {
+        private WebPage _wp;
+        public Game()
+        {
+            _wp = new("https://www.google.com/");
+
+
+        }
+    }
+}
---
+++ RedactleNet/Model/Webpage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedactleNet.Model
+{
+    public class WebPage
+    {
+        public string WebData { get; private set; } 
+        public WebPage(string webAddress)
+        {
+            System.Net.WebClient wc = new System.Net.WebClient();
+            WebData = wc.DownloadString(webAddress);
+        }
+    }
+}
