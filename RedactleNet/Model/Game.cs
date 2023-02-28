9f3933b1b615095c290f278c3b4d7a608b97f583
Marcin Gruszczyk <m.k.gruszczyk@gmail.com>
2023-02-28 20:59:26 +0100

    Added basic WebPage methods

--- RedactleNet/Model/Game.cs
+++ RedactleNet/Model/Game.cs
@@ -1,19 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace RedactleNet.Model
 {
     internal class Game
     {
+        private string _article;
+        private string _encryptedArticle;
+        public string EncryptedArticle { get { return _encryptedArticle; } }
+        private string _nameOfArticle;
         private WebPage _wp;
+        /// <summary>
+        /// Dictonary representing the guessed words. Key is the word, value is the number of hits.
+        /// </summary>
+        private Dictionary<List<string>, int> _guessedWords;
+        /// <summary>
+        /// Dictonary representing the guessed words. Key is the word, value is the number of hits.
+        /// </summary>
+        public Dictionary<List<string>, int> GuessedWords { get { return _guessedWords; } }
+
         public Game()
         {
-            _wp = new("https://www.google.com/");
-
-
+            _wp = new("https://en.wikipedia.org/wiki/United_States");
+            _nameOfArticle = _wp.GetArticleName();
+            _article = _wp.GetArticleContent();
+            _article = _article.Replace("(listen);", "");
         }
+
+        //private 
     }
 }
--- RedactleNet/Model/Webpage.cs
+++ RedactleNet/Model/Webpage.cs
@@ -1,18 +1,129 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace RedactleNet.Model
 {
     public class WebPage
     {
-        public string WebData { get; private set; } 
+        public string WebData { get; private set; }
+        
         public WebPage(string webAddress)
         {
-            System.Net.WebClient wc = new System.Net.WebClient();
-            WebData = wc.DownloadString(webAddress);
+            using var client = new HttpClient();
+
+            var content = client.GetStringAsync(webAddress);
+            WebData = content.Result;
         }
+         
+        public string GetArticleName()
+        {
+            string artName = string.Empty;
+            using (StringReader reader = new StringReader(WebData))
+            {
+                do
+                {
+                    artName = reader.ReadLine();
+                    if (artName != null)
+                    {
+                        if (artName.StartsWith("<title>"))
+                        {
+                            artName = artName.Replace("<title>", string.Empty);
+                            artName = artName.Replace(" - Wikipedia</title>", string.Empty);
+                            break;
+                        }
+                    }
+                } while (artName != null);
+            }
+            return artName;
+        }
+        public string GetArticleContent()
+        {
+            string artContent = string.Empty;
+            using (StringReader reader = new StringReader(WebData))
+            {
+                string tempArtCont;
+                do
+                {
+                    tempArtCont = reader.ReadLine();
+                    if (tempArtCont == null)
+                        break;
+
+                    if (tempArtCont.StartsWith("<p>") == false)
+                        continue;
+
+                    bool isHTMLStyleMarked = false;
+                    string ASCIINumbers = string.Empty;
+                    foreach (char c in tempArtCont)
+                    {
+                    # region getting rid of HTML style formating.
+                        // getting rid of HTML style formating.
+                        if (c == '<')
+                            isHTMLStyleMarked = true;
+                        else if (c == '>')
+                        {
+                            isHTMLStyleMarked = false;
+                            continue; // necessary, since it would add '>' into the text.
+                        }
+
+                        if (isHTMLStyleMarked)
+                            continue;
+                        #endregion
+
+                        #region deleting "&#32;" and so on.
+                        if (c == '&')
+                        {
+                            ASCIINumbers += c;
+                            continue;
+                        }
+                        else if (c == '#' && ASCIINumbers == "&")
+                        {
+                            ASCIINumbers += c;
+                            continue;
+                        }
+                        if (ASCIINumbers.Contains("&#"))
+                        {
+                            ASCIINumbers += c;
+                            if (ASCIINumbers.Contains("160;")) // non-brekable space
+                            {
+                                ASCIINumbers = string.Empty;
+                                artContent += " ";
+                                continue;
+                            }
+                            if (ASCIINumbers.Contains("8211;")) // sign "-"
+                            {
+                                ASCIINumbers = string.Empty;
+                                artContent += "- ";
+                                continue;
+                            }
+                            else if (ASCIINumbers.Contains("&#91;"))
+                            {
+                                if (ASCIINumbers.Contains("93;"))
+                                    ASCIINumbers = string.Empty;
+                                continue;
+                            }
+                            else if (c == ';' && ASCIINumbers.Contains("&#"))
+                            {
+                                ASCIINumbers= string.Empty;
+                                continue;
+                            }    
+                            continue;
+                        }
+                        #endregion
+
+                        artContent += c;
+                    }
+                    artContent += "\n";
+                } while (tempArtCont != null);
+            }
+            return artContent;
+
+        }
+
     }
 }
